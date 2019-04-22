d45e55fceb53f2988d88443cf0cedca9b4748b78
Daniel Levett <dlevett@hotmail.co.uk>
2019-04-22 18:08:19 +0100

    Fix default exports not re-exporting

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-export * from './addTime';
-export * from './subtractTime';
-export * from './multiplyTime';
-export * from './divideTime';
+export { default as addTime } from './addTime';
+export { default as subtractTime } from './subtractTime';
+export { default as multiplyTime } from './multiplyTime';
+export { default as divideTime } from './divideTime';
 
-export * from './normalizeTime';
-export * from './parseISODuration';
+export { default as normalizeTime } from './normalizeTime';
+export { default as parseISODuration } from './parseISODuration';
 
-export * from './toMilliseconds';
-export * from './toSeconds';
-export * from './toMinutes';
-export * from './toHours';
-export * from './toDays';
-export * from './toWeeks';
-export * from './toMonths';
-export * from './toYears';
+export { default as toMilliseconds } from './toMilliseconds';
+export { default as toSeconds } from './toSeconds';
+export { default as toMinutes } from './toMinutes';
+export { default as toHours } from './toHours';
+export { default as toDays } from './toDays';
+export { default as toWeeks } from './toWeeks';
+export { default as toMonths } from './toMonths';
+export { default as toYears } from './toYears';
 
 export * from './types';
